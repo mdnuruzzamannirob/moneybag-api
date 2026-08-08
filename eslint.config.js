c0e7d1721908a413a6aebbde7d3e7b8fc4e298ee
Md. Nuruzzaman <dev.mdnuruzzaman@gmail.com>
2026-08-08 22:32:25 +0600

    fix: update table of contents with proper markdown links

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
-
 export default [
   js.configs.recommended,
   {
